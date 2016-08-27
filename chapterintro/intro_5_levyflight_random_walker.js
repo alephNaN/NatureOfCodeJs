d901d6d05c260f2e8fa29e32f798327a5b798c09
Tuan Vo <automatwon@gmail.com>
2016-08-27 01:23:05 -0700

    Intro, section 5, Levy Flight Random Walker

---
+++ chapterintro/intro_5_levyflight_random_walker.js
@@ -0,0 +1,46 @@
+// Take bigs steps on the long tail
+class LevyFlightRandomWalker {
+  /* @param {int} x starting position
+   * @param {int} y starting position
+   */
+
+  constructor(x, y, isFancyWalker) {
+    this.x = x;
+    this.y = y;
+    // On scale of 0 to 1
+    this.LEVY_FLIGHT_PROBABILITY = .01;
+    this.SIZE = 3;
+  }
+
+  step() {
+    const r = random();
+    let stepX, stepY;
+    if ( r < this.LEVY_FLIGHT_PROBABILITY) {
+      stepX = random(-100, 100);
+      stepY = random(-100, 100);
+    } else {
+      stepX = random(-1, 1);
+      stepY = random(-1, 1);
+    }
+    this.x += stepX;
+    this.y += stepY;
+    console.log("X: " + this.x);
+  }
+
+  display() {
+    noStroke();
+    fill(0);
+    rect(this.x, this.y, this.SIZE, this.SIZE);
+  }
+}
+
+let w;
+function setup() {
+  createCanvas(640, 360);
+  w = new LevyFlightRandomWalker(width/2, height/2, true);
+  background(255);
+}
+function draw() {
+  w.step();
+  w.display();
+}
