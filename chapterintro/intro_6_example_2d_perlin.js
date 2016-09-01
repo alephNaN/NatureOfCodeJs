35385d6251356c400f550af0fa241c92d6fc72f9
Tuan Vo <automatwon@gmail.com>
2016-09-01 03:37:06 -0700

    Intro, Example 6, 2d Perlin Noise

---
+++ chapterintro/intro_6_example_2d_perlin.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400,200);
+  background(255);
+
+  let xoff = 0.0;
+  loadPixels();
+  for (let x = 0; x < width; x++) {
+    let yoff = 0.0;
+    for (let y = 0; y < height; y++) {
+      const bright = map(noise(xoff,yoff),0,1,0,255);
+      set(x, y, color(bright));
+      yoff+=.01;
+    }
+    xoff+=.01;
+  }
+  updatePixels();
+}
