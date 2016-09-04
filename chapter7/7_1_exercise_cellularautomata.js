1e861f7d6a7a1ef4629fcbfce276953cfff9c3d3
Tuan Vo <automatwon@gmail.com>
2016-09-03 23:24:07 -0700

    Chapter 7, Exercise 1, Cellular Automata Rule iteration

---
+++ chapter7/7_1_exercise_cellularautomata.js
@@ -0,0 +1,149 @@
+const ROWS = 40;
+const COLS = 100;
+const CELL_SIZE = 10;
+
+
+const CA = (function(rows, cols, cellSize) {
+    const NUM_STATES_PER_CELL = 2;
+    const NEIGHBORHOOD_SIZE = 3;
+    const NUM_CONFIGURATIONS = Math.pow(NUM_STATES_PER_CELL,
+        NEIGHBORHOOD_SIZE);
+    const CONFIGURATIONS = (function() {
+      const configurations = [];
+      for(let i = 0; i < NUM_CONFIGURATIONS; i++) {
+        configurations.push(getBinaryString(i, NEIGHBORHOOD_SIZE));
+      }
+      return configurations;
+    })();
+    const grid = (function() {
+      const g = [];
+      let i = rows;
+      while (i--) {
+          let j = cols;
+          const row = [];
+          while (j--) {
+            row.push(0);
+          }
+          g.push(row);
+      }
+      return g;
+    })();
+    let generation = 1;
+     // 0 <= ruleNumber < NUM_CONFIGURATIONS
+    let ruleNumber = 130;
+    let ruleMapping;
+    initializeCurrentRule();
+
+    // Initialize all cells to zero
+    function initializeEmptyGrid() {
+      let i = rows;
+      while (i--) {
+        let j = cols;
+        while (j--) {
+          grid[i][j] = 0;
+        }
+      }
+    }
+
+    function getRuleMapping(rule, configurations) {
+        const ruleMapping = {};
+        configurations.forEach((e, i) => {
+          ruleMapping[e] = rule[i];
+        });
+        return ruleMapping;
+    }
+
+    function getBinaryString(ruleNumber, length) {
+      const configuration = (ruleNumber).toString(2);
+      const zeroPadding = (new Array(length-configuration.length+1)).join('0');
+      return zeroPadding + configuration;
+    }
+
+    function decToBinaryArray(ruleNumber) {
+      return getBinaryString(ruleNumber, NUM_CONFIGURATIONS).split('');
+    }
+
+    function initializeCurrentRule() {
+      initializeEmptyGrid();
+      const middleCell = Math.floor((cols)/2);
+      set(0, middleCell, 1);
+      ruleMapping = getRuleMapping(decToBinaryArray(ruleNumber),
+          CONFIGURATIONS);
+    }
+
+    function checkBounds(i, j) {
+      if (i >= rows.length || j >= cols.length) {
+        throw new Exception("index out of bounds");
+      }
+    }
+
+    function set(i, j, value) {
+      checkBounds();
+      grid[i][j] = value == 1 ? 1 : 0;
+    }
+
+    function get(i, j) {
+      checkBounds();
+      return grid[i][j];
+    }
+
+    function lookupRule(neighborhood) {
+      return ruleMapping[neighborhood.join("")];
+    }
+    function getNeighborhood(i, j) {
+        return [get(i, j-1), get(i, j),get(i, j+1)];
+    }
+
+    function generate() {
+      // Start new rule set
+      if(generation == rows) {
+        // Increment rule, going to zero if at max
+        ruleNumber = ((ruleNumber++) === NUM_CONFIGURATIONS - 1 ) ? 0 : ruleNumber;
+
+        initializeCurrentRule();
+        generation = 1;
+      } else {
+        let j = cols;
+        while (j--) {
+          if ( j > 0 && j < cols-1) {
+            const neighborhood = getNeighborhood(generation-1, j);
+            set(generation, j, lookupRule(neighborhood));
+          }
+        }
+      }
+      generation++;
+    }
+
+    function render() {
+      let i = rows;
+      while (i--) {
+        let j = cols;
+        while (j--) {
+          if (grid[i][j]) {
+            fill(0);
+          } else {
+            fill(255);
+          }
+          rect(cellSize*j, cellSize*i, cellSize, cellSize);
+        }
+      }
+    }
+
+    return {
+        render: render,
+        generate: generate,
+        wipe:  initializeEmptyGrid
+    };
+})(ROWS, COLS, CELL_SIZE);
+
+
+function setup() {
+  createCanvas(COLS*CELL_SIZE, ROWS*CELL_SIZE);
+  background(255);
+  frameRate(20);
+}
+
+function draw() {
+  CA.generate();
+  CA.render();
+}
