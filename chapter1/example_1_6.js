cb399935ce9ea997f0204073d43dd86b23c7f264
Tuan Vo <automatwon@gmail.com>
2016-09-10 04:39:37 -0700

    Chapter 1, Example 6, Vector Normalization to Unit

---
+++ chapter1/example_1_6.js
@@ -0,0 +1,49 @@
+function PVector(x_, y_) {
+  this.x = x_;
+  this.y = y_;
+}
+PVector.prototype.add = function(o) {
+  this.x += o.x;
+  this.y += o.y;
+};
+PVector.prototype.sub = function(o) {
+  this.x -= o.x;
+  this.y -= o.y;
+};
+PVector.prototype.scalar = function(n) {
+  this.x *= n;
+  this.y *= n;
+};
+PVector.prototype.div = function(n) {
+  this.x /= n;
+  this.y /= n;
+};
+PVector.prototype.mag = function() {
+  return Math.sqrt(this.x*this.x + this.y*this.y);
+}
+PVector.prototype.normalize = function() {
+  const m = this.mag();
+  if ( m !== 0 ) {
+    this.div(m);
+  }
+}
+
+function setup() {
+  createCanvas(640, 360);
+}
+function draw() {
+  background(255);
+  const mouse  = new PVector(mouseX,mouseY);
+  const center = new PVector(width/2,height/2);
+  mouse.sub(center);
+  mouse.normalize();
+  mouse.scalar(50);
+
+  // Draws line for magnitudeo of vector
+  let m = mouse.mag();
+  fill(0);
+  rect(0, 0, m, 10);
+
+  translate(width/2, height/2);
+  line(0,0,mouse.x,mouse.y);
+}
